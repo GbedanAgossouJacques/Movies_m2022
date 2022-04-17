11f924ae1d320fd955188d285e0414261a757bec
Jacques Gbedan <gbedanjacques7@gmail.com>
2022-04-17 18:32:37 +0100

    ajout d'un controller

---
+++ Controllers/MoviesController1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movies_m2022.Controllers
+{
+    public class MoviesController1 : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
