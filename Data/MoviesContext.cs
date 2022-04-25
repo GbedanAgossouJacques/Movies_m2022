2377004d8886863dfddeb9bd89e2d895de83e1c2
Jacques Gbedan <gbedanjacques7@gmail.com>
2022-04-25 13:11:50 +0100

    Migration.01

---
+++ Data/MoviesContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Movies_m2022.Models;
+
+namespace Movies_m2022.Data
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext (DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movies_m2022.Models.Movie> Movie { get; set; }
+    }
+}
